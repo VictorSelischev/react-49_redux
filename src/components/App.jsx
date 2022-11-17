8fa40d025babce6f3d90e25eb0da99fa4b37e247
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-17 14:37:39 +0200

    Create TaskForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Layout } from './Layout/Layout';
 import { AppBar } from './AppBar/AppBar';
+import { TaskForm } from './TaskForm/TaskForm';
 
 export const App = () => {
   return (
     <Layout>
       <AppBar />
+      <TaskForm />
     </Layout>
   );
 };
