617601c9ee9d8cd01330bad06cffa39b28bbe88c
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-17 15:32:47 +0200

    Creaete TaskList

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,5 +1,14 @@
-import css from './TaskList.module.css'
+import css from './TaskList.module.css';
+import { Task } from 'components/Task/Task';
 
 export const TaskList = () => {
-    return ();
-}
+  return (
+    <ul className={css.list}>
+      {[].map(task => (
+        <li key={task.id} className={css.listItem}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
