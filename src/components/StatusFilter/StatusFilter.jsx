a1b119ced214ce85e95895f4dcbc0c07b4032379
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-19 21:14:31 +0200

    Logic StatusFilter

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,16 +1,16 @@
 import { Button } from 'components/Button/Button';
 import css from './StatusFilter.module.css';
 import { useSelector } from 'react-redux';
-import { statusFilter } from 'redux/constants';
+import { statusFilters } from 'redux/constants';
 
 export const StatusFilter = () => {
   const filter = useSelector(state => state.filters.status);
 
   return (
     <div className={css.wrapper}>
-      <Button selected={filter === statusFilter.all}>All</Button>
-      <Button selected={filter === statusFilter.active}>Active</Button>
-      <Button selected={filter === statusFilter.completed}>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
