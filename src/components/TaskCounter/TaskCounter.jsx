9d6559473dd249fb7bfd637d5267a35b0539529b
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-19 21:15:10 +0200

    Logic TaskCounter

--- src/components/TaskCounter/TaskCounter.jsx
+++ src/components/TaskCounter/TaskCounter.jsx
@@ -1,10 +1,25 @@
 import css from './TaskCounter.module.css';
+import { useSelector } from 'react-redux';
 
 export const TaskCounter = () => {
-    return (
-        <div>
-            <p className={css.text}>Active: 0</p>
-            <p className={css.text}>Compled: 0</p>
-        </div>
-    );
-}
+  const tasks = useSelector(state => state.tasks);
+
+  const count = tasks.reduce(
+    (acc, task) => {
+      if (task.completed) {
+        acc.completed += 1;
+      } else {
+        acc.active += 1;
+      }
+      return acc;
+    },
+    { active: 0, completed: 0 }
+  );
+
+  return (
+    <div>
+      <p className={css.text}>Active: {count.active}</p>
+      <p className={css.text}>Compled: {count.completed}</p>
+    </div>
+  );
+};
