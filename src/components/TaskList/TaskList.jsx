1b7a16d7d1bd891d3591358b268ffe4e7a23cc7a
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-19 21:14:53 +0200

    Logic TaskList

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,10 +1,30 @@
 import css from './TaskList.module.css';
 import { Task } from 'components/Task/Task';
+import { statusFilters } from 'redux/constants';
+import { useSelector } from 'react-redux';
+
+console.log(statusFilters);
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
 
 export const TaskList = () => {
+  const statusFilter = useSelector(state => state.filters.status);
+  const tasks = useSelector(state => state.tasks);
+
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+
   return (
     <ul className={css.list}>
-      {[].map(task => (
+      {visibleTasks.map(task => (
         <li key={task.id} className={css.listItem}>
           <Task task={task} />
         </li>
