e8e5ef1eb9120b805be2adb8a8fdb0a09d440dc7
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-22 18:31:18 +0200

    Create rootReducer

---
+++ src/redux/reducer.jsx
@@ -0,0 +1,27 @@
+import { statusFilters } from './constants';
+
+const initialState = {
+  tasks: [
+    { id: 0, text: 'Learn HTML and CSS', completed: true },
+    { id: 1, text: 'Get good at JavaScript', completed: true },
+    { id: 2, text: 'Master React', completed: false },
+    { id: 3, text: 'Discover Redux', completed: false },
+    { id: 4, text: 'Build amazing apps', completed: false },
+  ],
+  filters: {
+    status: statusFilters.all,
+  },
+};
+
+export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'tasks/addTask': {
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload],
+      };
+    }
+    default:
+      return state;
+  }
+};
