32e2849f3666011099800b0d0458431b1185019b
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-22 11:43:14 +0200

    Create handleToggle for checked

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,10 +1,15 @@
 import { Button } from 'components/Button/Button';
-import css from './TaskForm.module.css';    
+import css from './TaskForm.module.css';
+import { useDispatch } from 'react-redux';
+import { addTask } from 'redux/actions';
 
 export const TaskForm = () => {
+  const dispatch = useDispatch();
+
   const handleSubmit = e => {
     e.preventDefault();
     const form = e.target;
+    dispatch(addTask(form.elements.text.value))
     form.reset();
   };
 
