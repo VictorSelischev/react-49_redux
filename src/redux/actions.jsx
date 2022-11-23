d6df772357eee348502bd7ebb94982706e12531a
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-23 14:14:26 +0200

    createAction

--- src/redux/actions.jsx
+++ src/redux/actions.jsx
@@ -1,33 +1,67 @@
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
+import { createAction, nanoid } from '@reduxjs/toolkit';
 
-export const addTask = text => {
+export const addTask = createAction('tasks/addTask', text => {
   return {
-    type: 'tasks/addTask',
     payload: {
+      text,
       id: nanoid(),
       completed: false,
-      text,
     },
   };
-};
+});
 
-export const deleteTask = taskId => {
+export const deleteTask = createAction('tasks/deleteTask', taskId => {
   return {
-    type: 'tasks/deleteTask',
     payload: taskId,
   };
-};
+});
 
-export const toggleCompleted = taskId => {
+export const toggleCompleted = createAction('tasks/toggleCompleted', taskId => {
   return {
     type: 'tasks/toggleCompleted',
     payload: taskId,
   };
-};
+});
 
-export const setStatusFilter = value => {
-  return {
-    type: 'filters/setStatusFilter',
-    payload: value,
-  };
-};
+export const setStatusFilter = createAction(
+  'filters/setStatusFilter',
+  value => {
+    return {
+      type: 'filters/setStatusFilter',
+      payload: value,
+    };
+  }
+);
+
+// export const addTask = text => {
+//   return {
+//     type: 'tasks/addTask',
+//     payload: {
+//       id: nanoid(),
+//       completed: false,
+//       text,
+//     },
+//   };
+// };
+
+// export const deleteTask = taskId => {
+//   return {
+//     type: 'tasks/deleteTask',
+//     payload: taskId,
+//   };
+// };
+
+// export const toggleCompleted = taskId => {
+//   return {
+//     type: 'tasks/toggleCompleted',
+//     payload: taskId,
+//   };
+// };
+
+// export const setStatusFilter = value => {
+//   return {
+//     type: 'filters/setStatusFilter',
+//     payload: value,
+//   };
+// };
