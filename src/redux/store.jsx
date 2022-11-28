3937a61197a85e943d3f0b21c4b587117fb2af9e
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-11-28 21:33:05 +0200

    Update store.jsx

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -6,7 +6,9 @@
 // export const store = createStore(rootReducer, enhancer);
 
 import { configureStore } from "@reduxjs/toolkit";
-import { tasksReducer, filtersReducer } from "./reducer";
+// import { tasksReducer, filtersReducer } from "./reducer";
+import { tasksReducer } from "./tasksSlice";
+import { filtersReducer } from "./filtersSlice";
 
 export const store = configureStore({
     reducer: {
