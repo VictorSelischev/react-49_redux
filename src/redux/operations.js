354b69cb6c99b9e48230111a5ac9915cf6e2d3f5
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-14 20:36:14 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
+import {
+  fetchingInProgress,
+  fetchingSuccess,
+  fetchingError,
+} from './tasksSlice';
 
 axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
 
 const fetchTasks = () => async dispatch => {
-    try {
-        const response
-    }
-}
+  try {
+    dispatch(fetchingInProgress());
+    const response = await axios.get('/tasks');
+    dispatch(fetchingSuccess(response.data));
+  } catch (e) {
+    dispatch(fetchingError(e.message));
+  }
+};
