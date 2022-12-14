c5f2647ab651a29e410cc7471ab1087173a3abfa
Виктор Селищев <95778687+VictorSelischev@users.noreply.github.com>
2022-12-14 19:56:28 +0200

    create HTTP operations

--- src/redux/tasksSlice.jsx
+++ src/redux/tasksSlice.jsx
@@ -9,6 +9,32 @@ const tasksInitialState = [
   // { id: 4, text: 'Build amazing apps', completed: false },
 ];
 
+
+const tasksSlice = {
+  name: 'tasks',
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {
+    fetchingInProgress(state) { 
+      state.isLoading = true;
+    },
+    fetchingSuccess(state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
+     },
+    fetchingError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+};
+
+export const { fetchingInProgress, fetchingSuccess, fetchingError } = tasksSlice.actions;
+
 const tasksSlice = createSlice({
   name: 'tasks',
   initialState: tasksInitialState,
