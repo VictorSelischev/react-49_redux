6754e974cb136c2e526df0724ad4a4b21adfad83
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-16 20:31:02 +0200

    Indicator fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,18 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks } from 'redux/operations';
 import { getTasks } from 'redux/selectors';
+import { getError, getIsLoading } from 'redux/selectors';
 
 import { Layout } from './Layout/Layout';
 import { AppBar } from './AppBar/AppBar';
 import { TaskForm } from './TaskForm/TaskForm';
 import { TaskList } from './TaskList/TaskList';
 
+
 export const App = () => {
   const dispatch = useDispatch();
-  // const { items, isLoading, error } = useSelector(getTasks);
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
 
   useEffect(() => {
     dispatch(fetchTasks());
@@ -20,6 +23,7 @@ export const App = () => {
     <Layout>
       <AppBar />
       <TaskForm />
+      {isLoading && !error && <b>Request in progress...</b>}
       <TaskList />
     </Layout>
 
