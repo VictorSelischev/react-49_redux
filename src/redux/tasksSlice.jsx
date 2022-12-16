c24b981e79d658cb7b4be5e58296c7bd0d0ee698
Виктор Селищев <95778687+VictorSelischev@users.noreply.github.com>
2022-12-16 11:03:25 +0200

    Add extraReducers

--- src/redux/tasksSlice.jsx
+++ src/redux/tasksSlice.jsx
@@ -1,13 +1,15 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 // import { nanoid } from 'nanoid';
 
-const tasksInitialState = [
-  // { id: 0, text: 'Learn HTML and CSS', completed: true },
-  // { id: 1, text: 'Get good at JavaScript', completed: true },
-  // { id: 2, text: 'Master React', completed: false },
-  // { id: 3, text: 'Discover Redux', completed: false },
-  // { id: 4, text: 'Build amazing apps', completed: false },
-];
+import { fetchTasks } from './operations';
+
+// const tasksInitialState = [
+//   { id: 0, text: 'Learn HTML and CSS', completed: true },
+//   { id: 1, text: 'Get good at JavaScript', completed: true },
+//   { id: 2, text: 'Master React', completed: false },
+//   { id: 3, text: 'Discover Redux', completed: false },
+//   { id: 4, text: 'Build amazing apps', completed: false },
+// ];
 
 
 const tasksSlice = createSlice({
@@ -17,20 +19,32 @@ const tasksSlice = createSlice({
     isLoading: false,
     error: null,
   },
-  reducers: {
-    fetchingInProgress(state) { 
-      state.isLoading = true;
-    },
-    fetchingSuccess(state, action) {
+  extraReducers: {
+    [fetchTasks.pending](state) { state.isLoader = true; },
+    [fetchTasks.fulfilled](state, action) {
       state.isLoading = false;
-      state.error = null;
       state.items = action.payload;
-     },
-    fetchingError(state, action) {
+      state.error = null;
+    },
+    [fetchTasks.rejected](state, action) { 
       state.isLoading = false;
       state.error = action.payload;
     },
   },
+  // reducers: {
+  //   fetchingInProgress(state) { 
+  //     state.isLoading = true;
+  //   },
+  //   fetchingSuccess(state, action) {
+  //     state.isLoading = false;
+  //     state.error = null;
+  //     state.items = action.payload;
+  //    },
+  //   fetchingError(state, action) {
+  //     state.isLoading = false;
+  //     state.error = action.payload;
+  //   },
+  // },
 });
 
 export const { fetchingInProgress, fetchingSuccess, fetchingError } = tasksSlice.actions;
