8c91fa5d784bddfe03634b0e1992bb02545f2c27
Виктор Селищев <95778687+VictorSelischev@users.noreply.github.com>
2022-12-16 11:04:42 +0200

    Add thunkAPI in callback fetchTasks operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,10 +1,10 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import {
-  fetchingInProgress,
-  fetchingSuccess,
-  fetchingError,
-} from './tasksSlice';
+// import {
+//   fetchingInProgress,
+//   fetchingSuccess,
+//   fetchingError,
+// } from './tasksSlice';
 
 axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
 
@@ -18,7 +18,14 @@ axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
 //   }
 // };
 
-export const fetchTasks = createAsyncThunk('tasks/fetchAll', async () => {
-  const response = await axios.get('/tasks');
-  return response.data;
-});
+export const fetchTasks = createAsyncThunk(
+  'tasks/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/tasks');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
