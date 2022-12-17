68feb2387a4c67c16befedf5ecc62cd68721e431
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-17 17:18:03 +0200

    Add toggleCompleted in Task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,13 +2,13 @@ import { MdClose } from 'react-icons/md';
 import css from './Task.module.css';
 import { useDispatch } from 'react-redux';
 // import { deleteTask, toggleCompleted } from '../../redux/tasksSlice';
-import { deleteTask } from 'redux/operations';
+import { deleteTask, toggleCompleted } from 'redux/operations';
 
 export const Task = ({ task }) => {
   const dispatch = useDispatch();
 
   const handleDelete = () => dispatch(deleteTask(task.id));
-//   const handleToggle = () => dispatch(toggleCompleted(task.id));
+  const handleToggle = () => dispatch(toggleCompleted(task));
 
   return (
     <div className={css.wrapper}>
@@ -16,7 +16,7 @@ export const Task = ({ task }) => {
         type="checkbox"
         className={css.checkbox}
         checked={task.completed}
-        // onChange={handleToggle}
+        onChange={handleToggle}
       />
       <p className={css.text}>{task.text}</p>
       <button className={css.btn}
