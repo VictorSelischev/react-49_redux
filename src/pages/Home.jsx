904ffbf0c93f73f8b9706c5e3edd8b69cc33dc15
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-30 21:14:06 +0200

    Update Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+// const Home = () => {
+//     return (
+//         <h1>HomePage</h1>
+//     );
+// }
+
+// export default Home;
+
+
+
+export default function Home() {
+    return (
+        <div style={ }>
+            <h1 style={ } >
+                Task manager welcome page {' '}
+                <span role='img' aria-label='Greeting icon' >💁‍♀️</span>
+            </h1>
+        </div>
+    );
+}
