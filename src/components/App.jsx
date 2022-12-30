70ddec54252239edd9dff4d14e035f3e86096d25
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-30 19:17:18 +0200

    Routes in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
 import { useEffect, lazy } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
+
+import { Layout } from './Layout/Layout';
+
 import { fetchTasks } from 'redux/operations';
 import { selectTasks } from 'redux/selectors';
 import { selectError, selectIsLoading } from 'redux/selectors';
 
-import { Layout } from './Layout/Layout';
+
 import { AppBar } from './AppBar/AppBar';
 import { TaskForm } from './TaskForm/TaskForm';
 import { TaskList } from './TaskList/TaskList';
 
+// const MyComponent = lazy(() => import('path/to/MyComponent'));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -20,13 +25,24 @@ export const App = () => {
    }, [dispatch]);
 
   return (
-    <Layout>
-      <AppBar />
-      <TaskForm />
-      {isLoading && !error && <b>Request in progress...</b>}
-      <TaskList />
-    </Layout>
 
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Route>
+    </Routes>
+
+    // <Layout>
+    //   <AppBar />
+    //   <TaskForm />
+    //   {isLoading && !error && <b>Request in progress...</b>}
+    //   <TaskList />
+    // </Layout>
+
+    
     // <div>
     //   {isLoading && <p>Loading tasks...</p>}
     //   {error && <p>{error}</p>}
