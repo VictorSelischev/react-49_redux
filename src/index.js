28c61e5296945b8992e30dbc8bba58803999760b
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-30 15:34:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import { store, persistor } from 'redux/store';
-import 'modern-normalize';
+// import 'modern-normalize';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
