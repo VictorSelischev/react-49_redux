55a34fe1bb543627ab20a92517a6a5bdfd4a6cc6
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-30 21:55:11 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 import css from './Layout.module.css';
 
-export const Layout = ({ children }) => {
-  return <main className={css.container}>{children}</main>;
+export const Layout = () => {
+  return (
+    <div>
+      <Suspense>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
 };
